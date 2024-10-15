d887d75200109c9ef4d418ee91b4304e6876758c
gaiiaalessio <gaiuzza24@hotmail.it>
2024-10-15 16:36:48 +0200

    Parsing input

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,15 @@
 public class Calculator {
     public static void main(String[] args) {
+        String[] input = args[0].split ("");
+        String[] rank = new String[5];
+        String[] suite = new String[5];
+        for (int i=0; i<10; i=i+2) {
+            rank[i/2] = input[i];
+            suite[i/2] = input[i+1];
+        }
+
+        System.out.println(rank);
+        System.out.println(suite);
         // in input lista di (numero, seme)
         // funzione calcola punti, che chiamerà tutte le funzioni per i vari tipi di punto
     }
