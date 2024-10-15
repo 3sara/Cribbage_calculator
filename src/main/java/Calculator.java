13675a15939168e89335e8ceff4cd82e29e85c87
3sara <sara.cerpene@gmail.com>
2024-10-15 15:50:22 +0200

    calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public static void main(String[] args) {
+        // in input lista di (numero, seme)
+        // funzione calcola punti, che chiamerà tutte le funzioni per i vari tipi di punto
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // in input lista di (numero, seme)
+        // funzione calcola punti, che chiamerà tutte le funzioni per i vari tipi di punto
+    }
+}
+
+
